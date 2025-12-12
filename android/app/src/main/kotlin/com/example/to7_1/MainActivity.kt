1e5d6e6c534fb550317905590ddbd730935297b8
silvia.garciabouza <silvia.gar.bou.dev@gmail.com>
2025-12-12 21:30:41 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/to7_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.to7_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/Station.dart
@@ -0,0 +1,10 @@
+class Station{
+  final int last_updated,
+  final int num_bikes_available,
+  final List<> vehicle_docks_available,
+
+  Station({required this.last_updated, required this.num_bikes_available})
+  
+
+}
+//https://acoruna.publicbikesystem.net/customer/gbfs/v2/gl/station_status
