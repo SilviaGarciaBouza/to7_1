427a46a3e3b30223cf6ac60c683d1cec78f75407
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2026-01-16 15:02:58 +0100

    new test

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -2,34 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:to7_1/main.dart' as app;
-import 'package:to7_1/views/StationListScreen.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Prueba de sistema', () {
-    testWidgets(
-      'Flujo básico: cargar lista, buscar parada y navegación al detalle',
-      ((WidgetTester tester) async {
-        await tester.pumpWidget(const app.MyApp());
+    testWidgets('Cargar datos, buscar parada y navegar al detalle', ((
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const app.MyApp());
 
-        await tester.pumpAndSettle(const Duration(seconds: 3));
+      await tester.pumpAndSettle(const Duration(seconds: 3));
 
-        expect(find.text('BiciCoruña'), findsOneWidget);
+      expect(find.text('BiciCoruña'), findsOneWidget);
 
-        await tester.enterText(find.byType(TextField), 'Pontevedra');
-        await tester.pumpAndSettle();
+      await tester.enterText(find.byType(TextField), 'Pontevedra');
+      await tester.pumpAndSettle();
 
-        final paradaFinder = find.textContaining('Pontevedra');
-        expect(paradaFinder, findsAtLeastNWidgets(1));
+      final paradaFinder = find.textContaining('Pontevedra');
+      expect(paradaFinder, findsAtLeastNWidgets(1));
 
-        await tester.tap(paradaFinder.first);
-        await tester.pumpAndSettle();
+      await tester.tap(paradaFinder.first);
+      await tester.pumpAndSettle();
 
-        expect(find.byIcon(Icons.pedal_bike), findsWidgets);
-
-        expect(find.textContaining('Capacidad total'), findsOneWidget);
-      }),
-    );
+      // expect(find.byIcon(Icons.pedal_bike), findsOneWidget);
+      expect(find.text('Vacíos'), findsOneWidget);
+      expect(find.textContaining('estación'), findsOneWidget);
+    }));
   });
 }
